350211ae96ca53183bbde52c9036e94caeed9640
viminaria <viminaria@gmail.com>
2025-05-20 11:38:12 +0000

    add wsl clipboard

--- init.lua
+++ init.lua
@@ -26,9 +26,35 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+vim.opt.clipboard = 'unnamedplus' -- allows neovim to access the system clipboard
+
+-- Set wsl-clipboard for vim clipboard if running WSL
+-- Check if the current linux kernal is microsoft WSL version
+local function is_wsl()
+  local version_file = io.open('/proc/version', 'rb')
+  if version_file ~= nil and string.find(version_file:read '*a', 'microsoft') then
+    version_file:close()
+    return true
+  end
+  return false
+end
+
+-- If current linux is under WSL then use wclip.exe
+-- More info: https://github.com/memoryInject/wsl-clipboard
+if is_wsl() then
+  vim.g.clipboard = {
+    name = 'wsl-clipboard',
+    copy = {
+      ['+'] = 'wcopy',
+      ['*'] = 'wcopy',
+    },
+    paste = {
+      ['+'] = 'wpaste',
+      ['*'] = 'wpaste',
+    },
+    cache_enabled = true,
+  }
+end
 
 -- Enable break indent
 vim.opt.breakindent = true
